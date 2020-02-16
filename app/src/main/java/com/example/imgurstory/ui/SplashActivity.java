c44f38c2ae94134027cf1897f82067691ec77967
Ashish Mathur <ashish_1m@hotmail.com>
2020-02-16 19:46:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imgurstory/ui/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.imgurstory.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.imgurstory.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigateToMainActivity();
+            }
+        }, 3000);
+    }
+
+    private void navigateToMainActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
